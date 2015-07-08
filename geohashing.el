4c5bb735d664d0cc8e545368014ff2f84bbc70d6
Michael Eliachevitch <m.eliachevitch@gmx.net>
2015-07-08 21:23:44 +0200

    minor changes (eg code formatting)

--- geohashing.el
+++ geohashing.el
@@ -9,7 +9,7 @@ At the moment just an alias for car."
 
 (defun longitude (coordinates)
   "Takes coordinates as a list '(latitude longitude) [deg], returns longitude.
-At the moment just alias for cadr"
+At the moment just alias for cadr."
   (elt coordinates 1))
 
 (defun calendar-date-to-iso (calendar-date)
@@ -46,6 +46,7 @@ that calendar.el uses."
   (calendar-date-to-iso (calendar-current-date)))
 
 (defconst first-day-of-30W '(2008 05 27))
+
 (defun 30W-rule (coordinates date)
   "Determine if 30W rule has to be applied for coordinates.
 True if longitude east of -30deg and date is after (or on) 2008-05-27"
@@ -60,11 +61,9 @@ True if longitude east of -30deg and date is after (or on) 2008-05-27"
 
 (defun get-djia-string (date)
   "Takes date as list of '(year month day)
-and returns the Dow Jones Industrial Average as a string,
-if known for that date.
+and returns the Dow Jones Industrial Average as a string, if known for that date.
 In case that DJIA for that date is unkown, fails, as no error handling
-is implemented yet.
-Uses API at carabiner.peeron.com. "
+is implemented yet. Uses API at carabiner.peeron.com. "
   (cl-destructuring-bind (year month day) date
     (let* ((djia-url
             (format "http://carabiner.peeron.com/xkcd/map/data/%d/%02d/%02d"
@@ -86,19 +85,16 @@ Uses API at carabiner.peeron.com. "
   "Gets DJIA and calculates the md5 sum from the date and djia-string
 according to the geohashing algorithm, returns it as a lowercase hex string."
   (cl-destructuring-bind (year month day) date
-    (secure-hash
-     'md5 (format "%d-%02d-%02d-%s" year month day djia-string))))
+    (secure-hash 'md5 (format "%d-%02d-%02d-%s" year month day djia-string))))
 
 (defun geohash-offset (date 30W)
   "Takes date as a list of the form '(year month date) and boolean for 30W rule
 and returns the offset of the geohash of that date in respect to a graticule
 as a 2-value list."
-  (let* ((djia-string (get-djia-string (if 30W (+days date -1)
-                                         date)))
+  (let* ((djia-string (get-djia-string (if 30W (+days date -1) date)))
          (md5-string (get-md5-string date djia-string)))
     (cl-loop for (start end) in '((0 16) (16 32))
-             collect (hex-to-fractional
-                      (subseq md5-string start end)))))
+             collect (hex-to-fractional (subseq md5-string start end)))))
 
   (defun geohash-coordinates (graticule date)
     "Returns geohash coordinates for given graticule '(latitude longitudeg)
@@ -116,15 +112,13 @@ and date '(year month day) as a 2-value list '(latitude longitudeg)."
 Uses the haversine formula .
 http://en.wikipedia.org/wiki/Haversine_formula#The_haversine_formula"
     (let* ((earth-radius 6371)
-           (rcoord1 (mapcar (lambda (x) (* degrees-to-radians x))
-                            coord1))
-           (rcoord2 (mapcar (lambda (x) (* degrees-to-radians x))
-                            coord2))
+           (rcoord1 (mapcar (lambda (x) (degrees-to-radians x)) coord1))
+           (rcoord2 (mapcar (lambda (x) (degrees-to-radians x)) coord2))
            (delta (cl-mapcar '- rcoord2 rcoord1)))
-      (* 2 earth-radius
-         (asin (sqrt (+ (haversin (latitude delta))
-                        (* (cos (latitude rcoord1)) (cos (latitude rcoord2))
-                           (haversin (longitude delta)))))))))
+      (* 2 earth-radius (asin (sqrt (+ (haversin (latitude delta))
+                                       (* (cos (latitude rcoord1))
+                                          (cos (latitude rcoord2))
+                                          (haversin (longitude delta)))))))))
 
   (defun adjecent-graticules (home-coord)
     "Returns the coordinates of the four graticules which are nearest
@@ -140,13 +134,11 @@ Assumes rectangle graticules and might be problematic near the poles."
             (list (+ delta-lat lat) lon)
             (list (+ delta-lat lat) (+ delta-lon lon)))))
 
-  (defun minimize (fn-to-min list)
-    "Takes the function fn-to-min and applies it to each element of list.
+(defun minimize (fn-to-min list)
+  "Takes the function fn-to-min and applies it to each element of list.
 Returns the element of list for which fn-to-min gets minimal."
-    (second (reduce
-             (lambda (x y) (if (< (car x) (car y)) x y))
-             (mapcar (lambda (l) (list (funcall fn-to-min l) l))
-                     list))))
+  (second (reduce (lambda (x y) (if (< (car x) (car y)) x y))
+                  (mapcar (lambda (l) (list (funcall fn-to-min l) l)) list))))
 
   (defun calc-nearest-geohash (home-coords date)
     "Takes exact home coordinates as '(lat long) and date as '(year month day)
@@ -190,4 +182,4 @@ Intended for quick interactive use."
                      "Open in webbrowser? "))
         (browse-url osm-url))))
 
-  (provide 'geohashing)
+(provide 'geohashing)
