d6fbe70acd086803cbe1ac9a32e6ca45638efba7
Michael Eliachevitch <m.eliachevitch@posteo.de>
2022-08-15 18:05:46 +0200

    Cleanup: Don't use cl, start improving docstrings

--- geohashing.el
+++ geohashing.el
@@ -1,16 +1,15 @@
-(require 'cl)
+;; -*- lexical-binding: t; -*-
 (require 'calendar)
 (require 'org)
+(require 'cl-macs)
 
 (defun latitude (coordinates)
-  "Takes coordinates as a list '(latitude longitude) [deg], returns latitude.
-At the moment just an alias for car."
-  (elt coordinates 0))
+  "Take COORDINATES as a list '(latitude longitude) [deg]. Return latitude."
+  (car coordinates))
 
 (defun longitude (coordinates)
-  "Takes coordinates as a list '(latitude longitude) [deg], returns longitude.
-At the moment just alias for cadr."
-  (elt coordinates 1))
+  "Takes COORDINATES as a list '(latitude longitude) [deg]. Return longitude."
+  (cl-second coordinates))
 
 (defun calendar-date-to-iso (calendar-date)
   "Takes list '(month day year), which is the date order that
@@ -42,13 +41,13 @@ that calendar.el uses."
        days-to-add))))
 
 (defun date-today ()
-  "Returns today's date as list of the form '(year month day)."
+  "Return today's date as list of the form '(year month day)."
   (calendar-date-to-iso (calendar-current-date)))
 
 (defconst first-day-of-30W '(2008 05 27))
 
 (defun 30W-rule (coordinates date)
-  "Determine if 30W rule has to be applied for coordinates.
+  "Determine if 30W rule has to be applied for COORDINATES and given DATE.
 True if longitude east of -30deg and date is after (or on) 2008-05-27"
   (and (> (elt coordinates 0) -30)
        (not (date-compare date first-day-of-30W))))
@@ -56,130 +55,129 @@ True if longitude east of -30deg and date is after (or on) 2008-05-27"
 (30W-rule (list calendar-latitude calendar-longitude) (date-today))
 
 (defun hex-to-fractional (hex-string)
-  "Converts a lowercase hex string to a decimal fraction."
+  "Convert a lowercase HEX-STRING to a decimal fraction."
   (/ (string-to-number hex-string 16) (expt 16.0 (length hex-string))))
 
 (defun get-djia-string (date)
-  "Takes date as list of '(year month day)
-and returns the Dow Jones Industrial Average as a string, if known for that date.
-In case that DJIA for that date is unkown, fails, as no error handling
-is implemented yet. Uses API at carabiner.peeron.com. "
+  "Return DJIA string for DATE.
+Takes DATE as list of `(year month day)' and returns the Dow
+Jones Industrial Average as a string, if known for that date. In
+case that DJIA for that date is unkown, fails, as no error
+handling is implemented yet. Uses API at carabiner.peeron.com."
   (cl-destructuring-bind (year month day) date
     (let* ((djia-url
             (format "http://carabiner.peeron.com/xkcd/map/data/%d/%02d/%02d"
                     year month day))
            (buffer (url-retrieve-synchronously djia-url))
            (djia-string nil))
-      (save-excursion
-        (set-buffer buffer)
+      (with-current-buffer buffer
         (goto-char (point-min))
         (re-search-forward "^[0-9\.]+" nil 'move)
         (setq djia-string
               (buffer-substring-no-properties (point-at-bol) (point-at-eol)))
         (kill-buffer (current-buffer)))
       (if (equal "" djia-string)
-          (error "Couldn't get DJIA for that date.")
+          (error "Couldn't get DJIA for that date")
         djia-string))))
 
 (defun get-md5-string (date djia-string)
-  "Gets DJIA and calculates the md5 sum from the date and djia-string
-according to the geohashing algorithm, returns it as a lowercase hex string."
+  "Calculate the md5 hash string from the DATE and DJIA-STRING."
   (cl-destructuring-bind (year month day) date
     (secure-hash 'md5 (format "%d-%02d-%02d-%s" year month day djia-string))))
 
 (defun geohash-offset (date 30W)
-  "Takes date as a list of the form '(year month date) and boolean for 30W rule
-and returns the offset of the geohash of that date in respect to a graticule
-as a 2-value list."
+  "Calculate decimal points of geohash for DATE depending on 30W rule.
+Takes DATE as a list of the form '(year month date) and boolean
+for 30W rule and returns the offset of the geohash of that date
+in respect to a graticule as a 2-value list."
   (let* ((djia-string (get-djia-string (if 30W (+days date -1) date)))
          (md5-string (get-md5-string date djia-string)))
     (cl-loop for (start end) in '((0 16) (16 32))
              collect (hex-to-fractional (subseq md5-string start end)))))
 
-  (defun geohash-coordinates (graticule date)
-    "Returns geohash coordinates for given graticule '(latitude longitudeg)
-and date '(year month day) as a 2-value list '(latitude longitudeg)."
-    (let ((offset (geohash-offset date (30W-rule graticule date))))
-      (list (+ (latitude graticule) (latitude offset))
-            (+ (longitude graticule) (longitude offset)))))
+(defun geohash-coordinates (graticule date)
+  "Return geohash coordinates for given GRATICULE and DATE."
+  (let ((offset (geohash-offset date (30W-rule graticule date))))
+    (list (+ (latitude graticule) (latitude offset))
+          (+ (longitude graticule) (longitude offset)))))
 
-  (defun haversin (angle)
-    "Haversine function of an angle given in radians"
-    (/ (- 1 (cos angle)) 2))
+(defun haversin (angle)
+  "Haversine function of ANGLE given in radians.
+ See
+ https://en.wikipedia.org/wiki/Haversine_formula#The_haversine_formula"
+  (/ (- 1 (cos angle)) 2))
 
-  (defun calc-distance (coord1 coord2)
-    "Calculates the distance between two coordinates on earth.
-Uses the haversine formula .
-http://en.wikipedia.org/wiki/Haversine_formula#The_haversine_formula"
-    (let* ((earth-radius 6371)
-           (rcoord1 (mapcar (lambda (x) (degrees-to-radians x)) coord1))
-           (rcoord2 (mapcar (lambda (x) (degrees-to-radians x)) coord2))
-           (delta (cl-mapcar '- rcoord2 rcoord1)))
-      (* 2 earth-radius (asin (sqrt (+ (haversin (latitude delta))
-                                       (* (cos (latitude rcoord1))
-                                          (cos (latitude rcoord2))
-                                          (haversin (longitude delta)))))))))
+(defun calc-distance (coord1 coord2)
+  "Calculate the distance between two coordinates on earth COORD1 and COORD2."
+  (let* ((earth-radius 6371)
+         (rcoord1 (mapcar (lambda (x) (degrees-to-radians x)) coord1))
+         (rcoord2 (mapcar (lambda (x) (degrees-to-radians x)) coord2))
+         (delta (cl-mapcar '- rcoord2 rcoord1)))
+    (* 2 earth-radius (asin (sqrt (+ (haversin (latitude delta))
+                                     (* (cos (latitude rcoord1))
+                                        (cos (latitude rcoord2))
+                                        (haversin (longitude delta)))))))))
 
-  (defun adjecent-graticules (home-coord)
-    "Returns the coordinates of the four graticules which are nearest
-to the exact 'home' coordinates and should be searched for geohashes.
-home-coord should be a list of two reals numbers, '(lat lon).
-Assumes rectangle graticules and might be problematic near the poles."
-    (let* ((lat (floor (latitude home-coord)))
-           (lon (floor (longitude home-coord)))
-           (delta-lat (if (>= (mod lat 1) 0.5) 1 -1))
-           (delta-lon (if (>= (mod lon 1) 0.5) 1 -1)))
-      (list (list lat lon)
-            (list lat (+ delta-lon lon))
-            (list (+ delta-lat lat) lon)
-            (list (+ delta-lat lat) (+ delta-lon lon)))))
+(defun adjecent-graticules (home-coord)
+  "Return the coordinates of the four graticules nearest to HOME-COORD.
+These are which should be searched for the nearest geohash.
+Assumes rectangle graticules and might be problematic near the
+poles."
+  (let* ((lat (floor (latitude home-coord)))
+         (lon (floor (longitude home-coord)))
+         (delta-lat (if (>= (mod lat 1) 0.5) 1 -1))
+         (delta-lon (if (>= (mod lon 1) 0.5) 1 -1)))
+    (list (list lat lon)
+          (list lat (+ delta-lon lon))
+          (list (+ delta-lat lat) lon)
+          (list (+ delta-lat lat) (+ delta-lon lon)))))
 
 (defun minimize (fn-to-min list)
-  "Takes the function fn-to-min and applies it to each element of list.
+  "Take the function FN-TO-MIN and apply it to each element of LIST.
 Returns the element of list for which fn-to-min gets minimal."
-  (second (reduce (lambda (x y) (if (< (car x) (car y)) x y))
+  (cl-second (cl-reduce (lambda (x y) (if (< (car x) (car y)) x y))
                   (mapcar (lambda (l) (list (funcall fn-to-min l) l)) list))))
 
-  (defun calc-nearest-geohash (home-coords date)
-    "Takes exact home coordinates as '(lat long) and date as '(year month day)
+(defun calc-nearest-geohash (home-coords date)
+  "Takes exact home coordinates as '(lat long) and date as '(year month day)
 and returns list of coordinates of 'nearest geohash'.
 The 'nearest geohash' is determined by calculating the geohash locations
 for the 4 adjecant graticules and returning the geohash location with the
 smallest distance to the home coordinates as returned by calc-distance."
-    (let* ((offset (geohash-offset date (30W-rule home-coords date)))
-           (adjecent-grats (adjecent-graticules home-coords)))
-      (minimize
-       (lambda (hash-coords) (calc-distance hash-coords home-coords))
-       (mapcar (lambda (grat)
-                 (list (+ (latitude grat) (latitude offset))
-                       (+ (longitude grat) (longitude offset))))
-               adjecent-grats))))
+  (let* ((offset (geohash-offset date (30W-rule home-coords date)))
+         (adjecent-grats (adjecent-graticules home-coords)))
+    (minimize
+     (lambda (hash-coords) (calc-distance hash-coords home-coords))
+     (mapcar (lambda (grat)
+               (list (+ (latitude grat) (latitude offset))
+                     (+ (longitude grat) (longitude offset))))
+             adjecent-grats))))
 
-  (defun get-osm-url (coordinates &optional zoomlevel)
-    "Returns and OSM url to the given coordinates, with an optional OSM zoom level."
-    (cl-destructuring-bind (lat lon) coordinates
-      (if zoomlevel
-          (format "https://www.openstreetmap.org/?mlat=%f&mlon=%f#map=%d/%f/%f"
-                  lat lon zoomlevel lat lon)
+(defun get-osm-url (coordinates &optional zoomlevel)
+  "Returns and OSM url to the given coordinates, with an optional OSM zoom level."
+  (cl-destructuring-bind (lat lon) coordinates
+    (if zoomlevel
         (format "https://www.openstreetmap.org/?mlat=%f&mlon=%f#map=%d/%f/%f"
-                lat lon 10 lat lon))))
+                lat lon zoomlevel lat lon)
+      (format "https://www.openstreetmap.org/?mlat=%f&mlon=%f#map=%d/%f/%f"
+              lat lon 10 lat lon))))
 
-  (defun geohashing ()
-    "Uses the calendar-longitude and calendar-latitude variables and prompt
+(defun geohashing ()
+  "Uses the calendar-longitude and calendar-latitude variables and prompt
 the user for the date with the org-read-date function to calculate the nearest
 geohash coordinates for that date.
 Intended for quick interactive use."
-    (interactive)
-    (let* ((decoded-time (decode-time (org-read-date nil t)))
-           (date (reverse (cl-subseq decoded-time 3 6)))
-           (home-coords (list calendar-latitude calendar-longitude))
-           (geohash-coordinates (calc-nearest-geohash home-coords date))
-           (osm-url (get-osm-url geohash-coordinates)))
-      (when (yes-or-no-p
-             (format "Nearest geohash at %s\nwith a distance of %f km.\n%s"
-                     geohash-coordinates
-                     (calc-distance home-coords geohash-coordinates)
-                     "Open in webbrowser? "))
-        (browse-url osm-url))))
+  (interactive)
+  (let* ((decoded-time (decode-time (org-read-date nil t)))
+         (date (reverse (cl-subseq decoded-time 3 6)))
+         (home-coords (list calendar-latitude calendar-longitude))
+         (geohash-coordinates (calc-nearest-geohash home-coords date))
+         (osm-url (get-osm-url geohash-coordinates)))
+    (when (yes-or-no-p
+           (format "Nearest geohash at %s\nwith a distance of %f km.\nOpen in webbrowser?"
+                   geohash-coordinates
+                   (calc-distance home-coords geohash-coordinates)))
+      (browse-url osm-url))))
 
 (provide 'geohashing)
+;;; geohashing.el ends here
